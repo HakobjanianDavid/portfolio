87660ecc13fde8b5cc7e3d1e077e8da34d42a735
David <hakobjaniandavid@gmail.com>
2019-10-30 16:02:41 +0400

    init project, feat: add html, css, js

---
+++ script.js
@@ -0,0 +1,50 @@
+let submenu = document.querySelector('.submenu');
+
+document.getElementById('nav').onclick = function(event) {
+  let target = event.target;
+
+  if(target.className == 'title' || target.className == 'title_heading') {
+    if(submenu.style.display == '') {
+    open()
+    } else if (submenu.style.display = 'flex'){
+      close()
+    }
+  }
+  function open() {
+    submenu.style.display = 'flex';
+  }
+
+  function close() {
+    submenu.style.display = '';
+  }
+}
+
+
+let sliderIndex = 1;
+showSlide(sliderIndex);
+
+function plusSlides(n) {
+    showSlide(sliderIndex +=n);
+}
+
+
+function showSlide(n){
+    let slides = document.getElementsByClassName('mySlides');
+
+    for(let i=0; i<slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    if(sliderIndex > slides.length) {
+      sliderIndex = 1;
+    }
+
+    if(sliderIndex < 1) {
+      sliderIndex = slides.length;
+    }
+
+    slides[sliderIndex-1].style.display = 'block'; 
+}
+
+console.log(a);
+let a;
